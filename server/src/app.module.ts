d22c7b0796eee0be9442bc0de9398b4f208bd2cb
Tanmay Joshi <tanmayjoshi072@gmail.com>
2025-10-08 02:23:55 +0530

    New Design Added

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,11 +4,36 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Throttle,ThrottlerGuard,ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 @Module({
   imports: [AuthModule, UserModule,
-    MongooseModule.forRoot("mongodb://localhost:27017") 
+    MongooseModule.forRoot("mongodb://localhost:27017"),
+    ThrottlerModule.forRoot([
+        {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 20
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100
+      }
+    ]
+    ) 
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+    {
+      provide:APP_GUARD,
+      useClass:ThrottlerGuard
+    }
+  ],
 })
 export class AppModule {}
