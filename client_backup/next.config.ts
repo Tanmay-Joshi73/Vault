09939f25e8c19ea5aef51a42420774e9cbf19e31
Tanmay Joshi <124510117+Tanmay-Joshi73@users.noreply.github.com>
2025-10-18 14:14:13 +0530

    Delete client_backup directory

--- client_backup/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  outputFileTracingRoot: __dirname,  // ✅ This defines the correct root path
-};
-
-export default nextConfig;
